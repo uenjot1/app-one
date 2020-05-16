ef02d3db0bc36602196b81fa0dedcb89da473000
k2mobileqa <k2moblieqa@gmail.com>
2020-05-16 09:40:16 +0200

    empty app one

---
+++ src/main/java/unj/test/appone/appone/ApponeApplication.java
@@ -0,0 +1,13 @@
+package unj.test.appone.appone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApponeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApponeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unj/test/appone/appone/ApponeApplicationTests.java
@@ -0,0 +1,13 @@
+package unj.test.appone.appone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApponeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
