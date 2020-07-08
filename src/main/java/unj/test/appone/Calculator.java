dd08afa7158c4600063738d071e2f77e1d0c22c6
k2mobileqa <k2moblieqa@gmail.com>
2020-07-08 15:12:05 +0200

    test appp

---
+++ src/main/java/unj/test/appone/Calculator.java
@@ -0,0 +1,10 @@
+package unj.test.appone;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     public int sum(int a, int b) {
+          return a + b;
+     }
+}
